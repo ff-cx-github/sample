19b1118ebec514606e486c6f5c4d17b0498ee178
ff-cx-github <finbar.fleming@checkmarx.com>
2017-04-27 10:21:50 -0400

    Update Program.cs

--- VulnerableApp/Program.cs
+++ VulnerableApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,13 +21,39 @@ namespace VulnerableApp
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = connStr;
 
-            String cmdText = String.Format("insert into users values({0}, {1})", user, pwd);    
-            SqlCommand cmd = new SqlCommand(cmdText);
-
+            String cmdText = String.Format("insert into users values({0}, {1})", user, pwd);
             conn.Open();
+
+
+            SqlCommand cmd = new SqlCommand(cmdText);
             cmd.Connection = conn;
             cmd.ExecuteNonQuery();
+
+            SqlCommand cmd1 = new SqlCommand(cmdText);
+            cmd1.Connection = conn;
+            cmd1.ExecuteNonQuery();
+
+            SqlCommand cmd2 = new SqlCommand(cmdText);
+            cmd2.Connection = conn;
+            cmd2.ExecuteNonQuery();
+
+            SqlCommand cmd3 = new SqlCommand(cmdText);
+            cmd3.Connection = conn;
+            cmd3.ExecuteNonQuery();
+
+            SqlCommand cmd4 = new SqlCommand(cmdText);
+            cmd4.Connection = conn;
+            cmd4.ExecuteNonQuery();
+
+            SqlCommand cmd5 = new SqlCommand(cmdText);
+            cmd5.Connection = conn;
+            cmd5.ExecuteNonQuery();
+
             conn.Close();
+
+
+            Console.WriteLine("{0} - {1}", args[0], args[1]);
+            Console.WriteLine("{0} - {1}", user, pwd);
 
 
 
